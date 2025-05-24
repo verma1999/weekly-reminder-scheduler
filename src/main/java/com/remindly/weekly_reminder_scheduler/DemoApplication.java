22fe08a78c7e67567fb789fef9b1a004a9d64b17
Aditya Verma <verma.aditya1999@gmail.com>
2025-05-25 00:06:13 +0530

    Initial commit

---
+++ src/main/java/com/remindly/weekly_reminder_scheduler/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.remindly.weekly_reminder_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remindly/weekly_reminder_scheduler/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remindly.weekly_reminder_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
